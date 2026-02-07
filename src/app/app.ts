cbd2511945fb10d97d8f2225d32e9eabb8615696
AllSee1ng <rodionegor31@gmail.com>
2026-02-07 14:42:24 +0300

    auth page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from './shared/components/button/button.component';
-import { InputComponent } from './shared/components/input/input.component';
-import { PasswordInputComponent } from './shared/components/password-input/password-input.component';
-import { LogoComponent } from './shared/components/logo/logo.component';
+import { LayoutComponent } from './public/_layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, InputComponent, PasswordInputComponent, LogoComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
